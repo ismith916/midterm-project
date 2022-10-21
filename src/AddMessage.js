4d3081da0f97749a93ebc2cabcd618c462104672
Isaac Smith <103906316+ismith916@users.noreply.github.com>
2022-10-20 21:03:59 -0700

    Updated message

--- src/AddMessage.js
+++ src/AddMessage.js
@@ -2,16 +2,16 @@ import React, { useState } from 'react'
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
-const AddMessage = ({setMessages}) => {
-    // console.log(props.setMessages)
-    const [newName, setNewName] = useState("")
-    const [newMessage, setNewMessage] = useState("")
-    const handleSubmit = (e) => {
-e.preventDefault()
-setMessages(messages => [...messages, {name: newName, text: newMessage}])
-setNewName("")
-setNewMessage("")
-    }
+const AddMessage = ({ setMessages }) => {
+  // console.log(props.setMessages)
+  const [newName, setNewName] = useState("")
+  const [newMessage, setNewMessage] = useState("")
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setMessages(messages => [...messages, { name: newName, text: newMessage }])
+    setNewName("")
+    setNewMessage("")
+  }
   return (
     <Form className="mb-3" onSubmit={handleSubmit}>
       {/* <Form.Group className="mb-3" controlId="name">
@@ -21,7 +21,7 @@ setNewMessage("")
 
       <Form.Group className="mb-3" controlId="message">
         <Form.Label>"I am thinking of a number between 1 and 100. Guess the Lucky Number!"</Form.Label>
-        <Form.Control type="text" value={newMessage} onChange={(e) => {setNewMessage(e.target.value)}} />
+        <Form.Control type="text" value={newMessage} onChange={(e) => { setNewMessage(e.target.value) }} />
       </Form.Group>
       <Button variant="primary" type="submit">
         Guess
